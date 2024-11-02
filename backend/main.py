d57daec10f3c8c26bc16fb392cf3d9991ee7d0c1
Wred38 <nik.javor7@gmail.com>
2024-11-02 14:37:31 +0100

    Complete backend task + bonus 1

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from typing import Union
+from users import get_users
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
 
 app = FastAPI()
+users = get_users()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"]
+)
+
+class User(BaseModel):
+    firstName: str
+    lastName: str
+    age: int
+    gender: str
+    email: str
+    phone: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/users")
-def get_users():
-    # TODO ASSIGNMENT: Return a list of users
-    return []
+def get_users_list():
+    return {"users": users}
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User):
+    for the_user in users:
+        if the_user["id"] == user_id:
+            the_user.update(user.dict())
+            return the_user
+    raise HTTPException(status_code=404, detail="User not found")
